a77b3e230e720af46d2a547307890743abe532f9
krutm <krutmaster25@mail.ru>
2025-02-03 15:26:56 +0300

    MarkdownV2 for quote DeepSeek thinking

--- tg_bot.py
+++ tg_bot.py
@@ -5,17 +5,16 @@ from aiogram.filters import Command
 from secret import API_TOKEN
 from json import dumps as json_dumps
 from json import loads as json_loads
+import re
 
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+think_pattern = r"<think>(.*?)</think>"  # For DeepSeek thinking
 
 
 async def get_answered_text():
-    # Подключаемся к базе данных
     conn = connect_to_db()
-    # Открываем курсор для выполнения операций с БД
     cursor = conn.cursor()
     query = "select id, history from dialog where is_answered = %s and last_msg_id = %s limit 1;"
     params = ('true', '0',)
@@ -78,27 +77,54 @@ async def split_text_to_chunks(text, max_length=4095):
     return chunks
 
 
+def escape_md_v2_custom(text: str) -> str:
+    """
+    Экранирует текст для MarkdownV2, но оставляет нужные символы форматирования (*, _, > и т.д.)
+    """
+    escape_chars = r'([]()~`+=|{}.!-)'
+    return re.sub(r'(?<!\\)([' + re.escape(escape_chars) + r'])', r'\\\1', text)
+
+
+def convert_to_quote(match):
+    """
+    Заворачивает блок текста в цитату для re.sub()
+    """
+    inner_text = match.group(1).strip()
+    text_lines = inner_text.splitlines()
+    quoted_text = f"**>{text_lines[0]}\n"
+    quoted_text += "\n".join([">" + line for line in text_lines[1:]])
+    return quoted_text  # Без экранирования – оно будет позже
+
+
 async def send_answered_text(chat_id: int):
     """
     Функция отправки сообщения и вызова обновления диалога.
     """
-    # Получаем текст для отправки
     res_query = await get_answered_text()
     if res_query is not False:
         id_dialog = res_query[1]
         text_to_send = res_query[0]
+        text_to_send = re.sub(think_pattern, convert_to_quote, text_to_send, flags=re.DOTALL)
+        text_to_send = escape_md_v2_custom(text_to_send)
 
-        if len(text_to_send) > 4095:
+        if len(text_to_send) > 4094:
             text_to_send_chunks = await split_text_to_chunks(text_to_send)
-            for text_to_send in text_to_send_chunks:
+            for chunk in text_to_send_chunks:
+                await asyncio.sleep(3)
+                try:  # Обработчики на случай, если MdV2 даст ошибку экранирования
+                    sent_message = await bot.send_message(chat_id=chat_id, text=chunk, parse_mode='MarkdownV2')
+                except Exception as e:
+                    await bot.send_message(chat_id=chat_id, text=f'Ошибка при отправке с парсингом, попытка отправить без парсинга:\n{e}')
+                    await asyncio.sleep(3)
+                    sent_message = await bot.send_message(chat_id=chat_id, text=chunk)
+        else:
+            try:  # Обработчики на случай, если MdV2 даст ошибку экранирования
+                sent_message = await bot.send_message(chat_id=chat_id, text=text_to_send, parse_mode='MarkdownV2')
+            except Exception as e:
+                await bot.send_message(chat_id=chat_id, text=f'Ошибка при отправке с парсингом, попытка отправить без парсинга:\n{e}')
                 await asyncio.sleep(3)
                 sent_message = await bot.send_message(chat_id=chat_id, text=text_to_send)
-        else:
 
-            # Отправляем текст пользователю
-            sent_message = await bot.send_message(chat_id=chat_id, text=text_to_send)
-
-        # Обновляем диалог на основе ID отправленного сообщения
         await answer_update_dialog(id_dialog, sent_message.message_id)
 
 
@@ -123,7 +149,7 @@ async def periodic_task(chat_id: int):
     """
     while True:
         await send_answered_text(chat_id)
-        await asyncio.sleep(10)  # Интервал между отправкой сообщений (например, 10 секунд)
+        await asyncio.sleep(10)
 
 
 async def clear_db():
@@ -131,13 +157,11 @@ async def clear_db():
     Функция для запуска периодической отправки сообщений.
     """
     while True:
-        # Подключаемся к базе данных
         conn = connect_to_db()
-        # Открываем курсор для выполнения операций с БД
         cursor = conn.cursor()
         query = "delete from dialog where date < NOW() - INTERVAL '3 days';"
         execute_query(conn, cursor, query)
-        await asyncio.sleep(60*60*12)  # Интервал между отправкой сообщений (например, 10 секунд)
+        await asyncio.sleep(60*60*12)
 
 
 # async def send_welcome(message: types.Message):
@@ -151,8 +175,7 @@ async def main():
     asyncio.create_task(periodic_task(538231919))
     asyncio.create_task(clear_db())
 
-    # Запуск бота
-    await dp.start_polling(bot)  # Старт polling для получения обновлений
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
