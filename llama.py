ea36666921a7ffc66ec4db75eba0aa453fe1bd88
krutm <krutmaster25@mail.ru>
2025-03-19 04:02:23 +0300

    Fix llama; add log for time tracking

--- llama.py
+++ llama.py
@@ -1,11 +1,15 @@
 from openai import OpenAI
+import logging
+
+
+logging.basicConfig(filename='log_llama.log', level=logging.INFO, encoding='UTF-8', format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def request_to_lamma(dialog_history, start_prompt=None, is_initiation=True):
     # Подключение к локально запущенному OpenAI API
     client = OpenAI(
-        base_url='http://ollama:11434/v1',
-        # base_url='http://localhost:11434/v1',
+        # base_url='http://ollama:11434/v1',
+        base_url='http://localhost:11434/v1',
         api_key='ollama'
     )
 
@@ -14,25 +18,30 @@ def request_to_lamma(dialog_history, start_prompt=None, is_initiation=True):
 
     # Добавляем сообщение пользователя в историю диалога
     if is_initiation:
-        dialog_history = [{
+        dialog_history = [
+            {
+                "role": "system",
+                "content": "Отвечай исключительно на русском языке и строго следуй инструкциям."
+            },
+            {
             "role": "user",
             "content": ''
-                       f'Текст: {dialog_history}'
+                       f'{start_prompt} Текст: {dialog_history}'
         }]
 
-    print('\nЛама думает, ожидайте...')
     # Отправляем запрос к модели с текущей историей диалога
+    logging.info('Модель думает')
     response = client.chat.completions.create(
         model="deepseek-r1:8b",
         messages=dialog_history,
-        # temperature=0.3,
-        top_p=0.8,
-        frequency_penalty=0.3
+        temperature=0.3,
+        top_p=0.5,
+        frequency_penalty=0.5
     )
 
     # Извлекаем содержимое ответа модели
     response_content = response.choices[0].message.content
-    print("\nОтвет модели:", response_content)
+    logging.info('Модель ответила')
 
     # Добавляем ответ модели в историю диалога
     dialog_history.append({
